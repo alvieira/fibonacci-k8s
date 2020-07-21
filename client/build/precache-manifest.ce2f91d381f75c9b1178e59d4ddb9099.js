72f9cef93501eb599cd3c7a6413485177333a358
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-07-20 22:51:21 -0300

    first commit

---
+++ client/build/precache-manifest.ce2f91d381f75c9b1178e59d4ddb9099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e79a701f42e18a0988e952af78954d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecd1d73d258d7e9eb104",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67fb369ca3bc5d6ee523",
+    "url": "/static/js/2.05ef5b9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.05ef5b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd1d73d258d7e9eb104",
+    "url": "/static/js/main.d417f1eb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
